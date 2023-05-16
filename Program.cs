08b77c3eec1a50736c26c74e462d047cd39d5d67
Yasser Greyeb <Yasser.Greyeb@gmail.com>
2023-05-16 01:11:33 -0500

    Implement tray icon

--- Program.cs
+++ Program.cs
@@ -1,56 +1,7 @@
-using Bewake;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Timers;
+using System.Runtime.Versioning;
 using static Bewake.EXECUTION_STATE;
-
-[DllImport("kernel32.dll", SetLastError = true)]
-static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
-
-// keep state to prevent unnecessary calls to win api
-bool awake = false;
-
-// create timer to check the number of screens at intervals and prevent screen sleep accordingly
-const int CheckInterval = 1000; // ms
-System.Timers.Timer timer = new(CheckInterval);
-timer.Elapsed += CheckScreens;
-// start the timer
-timer.Enabled = true;
-
-// catch program flow
-Console.WriteLine("Press the Enter key to exit the program at any time... ");
-Console.ReadLine();
-
-Console.WriteLine("Release display state before exit");
-ResetState();
-
-void CheckScreens(object? sender, ElapsedEventArgs e)
-{
-    if (Screen.AllScreens.Length > 1)
-    {
-        if (!awake)
-        {
-            Console.WriteLine($"Keep display awake on {Screen.AllScreens.Length} screens");
-            awake = SetState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
-        }
-    }
-    else if (awake)
-    {
-        Console.WriteLine("Release display state for single screen");
-        awake = !ResetState();
-    }
-}
-
-static bool ResetState()
-{
-    return SetState(ES_CONTINUOUS);
-}
-
-static bool SetState(EXECUTION_STATE flags)
-{
-    bool succeeded = SetThreadExecutionState(flags) != 0;
-    if (!succeeded) Console.WriteLine($"Error: unable to set thread execution state [{flags}]");
-    return succeeded;
-}
 
 namespace Bewake
 {
@@ -59,5 +10,98 @@ namespace Bewake
     {
         ES_CONTINUOUS = 0x80000000,
         ES_DISPLAY_REQUIRED = 0x00000002
+    }
+
+    internal static partial class Program
+    {
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        private static partial EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        [SupportedOSPlatform("windows")]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+
+            Container container = new();
+
+            ContextMenuStrip contextMenu = new(container);
+            contextMenu.Items.Add("E&xit", null, ExitApplication);
+
+            Icon offIcon = new(@"ico\off.ico");
+            Icon onIcon = new(@"ico\on.ico");
+
+            NotifyIcon trayIcon = new(container)
+            {
+                Icon = offIcon,
+                Text = "Bewake is iddle",
+                ContextMenuStrip = contextMenu,
+                Visible = true
+            };
+
+            // keep state to prevent unnecessary calls to win api
+            bool awake = false;
+
+            // timer to check number of screens at intervals and prevent screen sleep
+            const int CheckInterval = 1000; // ms
+            System.Windows.Forms.Timer timer = new(container)
+            {
+                Interval = CheckInterval
+            };
+            timer.Tick += CheckScreens;
+            timer.Start();
+
+            Application.Run();
+
+            void CheckScreens(object? sender, EventArgs e)
+            {
+                if (Screen.AllScreens.Length > 1)
+                {
+                    if (!awake)
+                    {
+                        Console.WriteLine($"Keep display awake on {Screen.AllScreens.Length} screens");
+                        if (SetState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED))
+                        {
+                            awake = true;
+                            trayIcon.Icon = onIcon;
+                            trayIcon.Text = "Bewake is active";
+                        }
+                    }
+                }
+                else if (awake)
+                {
+                    Console.WriteLine("Release display state for single screen");
+                    if (ResetState())
+                    {
+                        awake = false;
+                        trayIcon.Icon = offIcon;
+                        trayIcon.Text = "Bewake is iddle";
+                    }
+                }
+            }
+        }
+
+        static bool ResetState()
+        {
+            return SetState(ES_CONTINUOUS);
+        }
+
+        static bool SetState(EXECUTION_STATE flags)
+        {
+            bool succeeded = SetThreadExecutionState(flags) != 0;
+            if (!succeeded) Console.WriteLine($"Error: unable to set thread execution state [{flags}]");
+            return succeeded;
+        }
+
+        static void ExitApplication(object? sender, EventArgs e)
+        {
+            Application.Exit();
+
+            Console.WriteLine("Release display state before exit");
+            ResetState();
+        }
     }
 }
